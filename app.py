b46137a0941dc9fc29cc34856e86b37c2e6b8232
rupan03 <157920733+rupan03@users.noreply.github.com>
2025-06-08 16:25:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,17 +28,3 @@ def preprocess_text(text):
 # Streamlit app
 st.title("📱 Smartphone Review Sentiment Analyzer")
 review = st.text_area("Enter your review:")
-
-from huggingface_hub import hf_hub_download
-import pickle
-
-# Download model
-model_path = hf_hub_download(repo_id="mythic003/sentiment-analysis-model", filename="lr_model.pkl")
-tfidf_path = hf_hub_download(repo_id="mythic003/sentiment-analysis-model", filename="tfidf.pkl")
-
-# Load model
-with open(model_path, "rb") as f:
-    model = pickle.load(f)
-
-with open(tfidf_path, "rb") as f:
-    tfidf = pickle.load(f)
